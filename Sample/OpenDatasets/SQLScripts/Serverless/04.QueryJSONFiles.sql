8bc28d3e1851bea68e9f2da65ac7f37e98687596
fabragaMS <50192911+fabragaMS@users.noreply.github.com>
2022-03-25 14:54:07 +1000

    Sample Code Enhancements

--- Sample/OpenDatasets/SQLScripts/Serverless/04.QueryJSONFiles.sql
+++ Sample/OpenDatasets/SQLScripts/Serverless/04.QueryJSONFiles.sql
@@ -3,7 +3,7 @@
 /* Read JSON files */
 SELECT TOP 10 *
 FROM OPENROWSET(
-        BULK 'https://pandemicdatalake.blob.core.windows.net/public/curated/covid-19/ecdc_cases/latest/ecdc_cases.jsonl',
+        BULK 'https://pandemicdatalake.blob.core.windows.net/public/curated/covid-19/ecdc_cases/latest/ecdc_cases.json',
         FORMAT = 'csv',
         FIELDTERMINATOR ='0x0b',
         FIELDQUOTE = '0x0b'
